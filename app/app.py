aaa1c25dac890b2d596dbb11ffdc8c7370271131
Bakley <barclaykoin@gmail.com>
2018-07-18 11:59:47 +0300

    [feature #159102182] Built Method to get all entries

--- app/app.py
+++ app/app.py
@@ -1,25 +1,35 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, jsonify
+# from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
 Dashboard = [
     {
+        "id" : 1,
         "Date" : "Tuesday",
         "Title" : "My first Blog",
         "Body" : "So I decided to pin down all my thoughts here...."
     },
 
     {
+        "id" : 2,
         "Date" : "Wednesday",
         "Title" : "My WCW",
         "Body" : "Ain't she a beauty...."
     },
 
     {
+        "id" : 3,
         "Date" : "Thursday",
         "Title" : "Throw Back",
         "Body" : "So when is it i decided to try programming again?...." 
     }
 ]
+
+@app.route('/mydiary/api/v1/entries', methods=['GET'])
+def get_entries():
+    return jsonify({'Dashboard': Dashboard})
+
+if __name__ == "__main__":
+    app.run(debug=True)
